232004d948f57e85fc7154289216bd1412ab00c8
Cleanderson Lins <kleeanlins@live.com>
2018-09-06 15:01:00 -0300

    Control layout update

--- app/src/main/java/com/example/cleanderson/baleasy_java/ControlActivity.kt
+++ app/src/main/java/com/example/cleanderson/baleasy_java/ControlActivity.kt
@@ -1,10 +1,60 @@
 package com.example.cleanderson.baleasy_java
 
+import android.app.ProgressDialog
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothSocket
+import android.content.Context
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import java.util.*
 
 class ControlActivity: AppCompatActivity(){
+
+    companion object {
+        var m_myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+        var m_bluetoothSocket: BluetoothSocket? = null
+        lateinit var m_progress: ProgressDialog
+        lateinit var m_bluetoothAdapter: BluetoothAdapter
+        var m_isConnecterd: Boolean = false
+        lateinit var m_address: String
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.control_layout)
+        m_address = intent.getStringExtra(SelectDeviceActivity.EXTRA_ADDRESS)
+
+        ConnectToDevice(this).execute()
+    }
+
+    private fun sendCommand(input: String){
+
+    }
+
+    private fun disconnect(){
+
+    }
+
+    private class ConnectToDevice(c: Context): AsyncTask<Void, Void, String>(){
+
+        private var connectSuccess: Boolean = true
+        private val context: Context
+
+        init {
+            this.context = c
+        }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+
+        override fun doInBackground(vararg params: Void?): String? {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
     }
 }
