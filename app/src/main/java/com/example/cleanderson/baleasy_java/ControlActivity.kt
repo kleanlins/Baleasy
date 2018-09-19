f28fd93f9c6d5559a85c5f81c4936a8fbada3d10
Cleanderson Lins <cleandersonlins@gmail.com>
2018-09-19 14:54:13 -0300

    Background task using RxJava initial implementation

--- app/src/main/java/com/example/cleanderson/baleasy_java/ControlActivity.kt
+++ app/src/main/java/com/example/cleanderson/baleasy_java/ControlActivity.kt
@@ -9,18 +9,21 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.widget.Toast
+import com.github.davidmoten.rx2.Strings
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.control_layout.*
 import org.jetbrains.anko.doAsync
 import java.io.IOException
-import java.nio.charset.Charset
+import java.io.InputStream
+import java.io.InputStreamReader
 import java.util.*
-import kotlin.collections.ArrayList
+import java.util.concurrent.TimeUnit
 
-class ControlActivity: AppCompatActivity(){
+class ControlActivity : AppCompatActivity() {
 
     companion object {
-
         var m_myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
         var m_bluetoothSocket: BluetoothSocket? = null
         lateinit var m_progress: ProgressDialog
@@ -36,56 +39,71 @@ class ControlActivity: AppCompatActivity(){
 
         ConnectToDevice(this).execute()
 
-        control_led_on.setOnClickListener{ sendCommand( "a") }
-        control_led_off.setOnClickListener{ sendCommand( "b") }
-        control_led_disconnect.setOnClickListener{ disconnect() }
-
-//        readFromBluetooth()
-
+        control_led_on.setOnClickListener { sendCommand("a") }
+        control_led_off.setOnClickListener { sendCommand("b") }
+        control_led_disconnect.setOnClickListener { disconnect() }
 
     }
 
-    private fun sendCommand(input: String){
+    private fun sendCommand(input: String) {
         val inputSt = m_bluetoothSocket!!.inputStream
+//        val scanner = Scanner(inputSt)
         val inputAsStream: String
+        val reader = InputStreamReader(inputSt)
 
-        if(m_bluetoothSocket != null){
-            try{
+        if (m_bluetoothSocket != null) {
+            try {
                 m_bluetoothSocket!!.outputStream.write(input.toByteArray())
 
                 val array = ByteArray(15)
                 var arrayString: String? = ""
 
-                val leitura = m_bluetoothSocket!!.inputStream.read(array)
+                /*val reading = List(4) {
+                    scanner.nextInt()
+                }*/
 
-                Toast.makeText(applicationContext, array.joinToString(separator = " "), Toast.LENGTH_SHORT).show()
-                Log.i("device", ""+array.joinToString(separator = " "))
+                /*val reading = scanner.nextLine()
 
-                for(i in 0..14){
+                val numbers = reading.split(' ').map {
+                    it.toInt()
+                }*/
+
+//                Log.i("device", numbers.joinToString(" "))
+
+                /*do {
+                    val reading = m_bluetoothSocket!!.inputStream.read(array)
+                }while(array[3].toInt() != 32)
+*/
+
+
+//                Toast.makeText(applicationContext, array.joinToString(separator = " "), Toast.LENGTH_SHORT).show()
+//                Log.i("device", ""+array.joinToString(separator = " "))
+
+                /*for(i in 0..14){
                     arrayString += array[i].toChar()
                 }
-                Log.i("bytesToString", ""+arrayString)
+                Log.i("bytesToString", ""+arrayString)*/
 
 
-            }catch(e: IOException){
+            } catch (e: IOException) {
                 e.printStackTrace()
             }
         }
     }
 
-    private fun disconnect(){
-        if(m_bluetoothSocket != null) {
+    private fun disconnect() {
+        if (m_bluetoothSocket != null) {
             try {
                 m_bluetoothSocket!!.close()
                 m_bluetoothSocket = null
                 m_isConnected = false
-            }catch(e: IOException){
+            } catch (e: IOException) {
                 e.printStackTrace()
             }
         }
     }
 
-    private fun readFromBluetooth(){
+    private fun readFromBluetooth() {
 
         doAsync {
             while (m_isConnected) {
@@ -95,16 +113,16 @@ class ControlActivity: AppCompatActivity(){
 
                 m_bluetoothSocket!!.inputStream.read(array)
 
-                for(i in 0..14){
+                for (i in 0..14) {
                     arrayString += array[i].toChar()
                 }
-                Log.i("bytesToString", ""+arrayString)
+                Log.i("bytesToString", "" + arrayString)
 
             }
         }
     }
 
-    private class ConnectToDevice(c: Context): AsyncTask<Void, Void, String>(){
+    private class ConnectToDevice(c: Context) : AsyncTask<Void, Void, String>() {
 
         private var connectSuccess: Boolean = true
         private val context: Context
@@ -120,14 +138,14 @@ class ControlActivity: AppCompatActivity(){
 
         override fun doInBackground(vararg params: Void?): String? {
             try {
-                if(m_bluetoothSocket == null || !m_isConnected){
+                if (m_bluetoothSocket == null || !m_isConnected) {
                     m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
                     val device: BluetoothDevice = m_bluetoothAdapter.getRemoteDevice(m_address)
                     m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(m_myUUID)
                     BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
                     m_bluetoothSocket!!.connect()
                 }
-            }catch (e: IOException){
+            } catch (e: IOException) {
                 connectSuccess = false
                 e.printStackTrace()
             }
@@ -137,12 +155,39 @@ class ControlActivity: AppCompatActivity(){
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-            if(!connectSuccess){
+            if (!connectSuccess) {
                 Log.i("DATA", "Coudn't connect.")
-            }else{
+            } else {
                 m_isConnected = true
+                createObservable(m_bluetoothSocket!!.inputStream)
             }
             m_progress.dismiss()
         }
     }
+
+}
+
+private fun createObservable(inputStream: InputStream) {
+    val reader = InputStreamReader(inputStream)
+    val scanner = Scanner(inputStream)
+    val reading = scanner.nextLine()
+    val sensors = reading.split(' ').map{
+        it.toInt()
+    }
+
+    Observable.fromArray(sensors)
+            .delay(100, TimeUnit.MILLISECONDS)
+            .subscribeOn(Schedulers.computation())
+            .subscribe{
+                Log.i("device", sensors.joinToString(" "))
+            }
+
+    /*Strings.from(reader, 15)
+            .map { it.split(" ")
+    }.delay(100, TimeUnit.MILLISECONDS)
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { numbers ->
+                Log.i("device", numbers.joinToString(" "))
+            }*/
 }
