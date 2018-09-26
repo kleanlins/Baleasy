241e482ca1fb5bcb3e10d10bbf13905c086357f4
Cleanderson Lins <cleandersonlins@gmail.com>
2018-09-26 15:37:58 -0300

    Main interface changes

--- app/src/main/java/com/example/cleanderson/baleasy_java/ControlActivity.kt
+++ app/src/main/java/com/example/cleanderson/baleasy_java/ControlActivity.kt
@@ -1,6 +1,5 @@
 package com.example.cleanderson.baleasy_java
 
-import android.animation.ObjectAnimator
 import android.app.ProgressDialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
@@ -10,17 +9,14 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.Window
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.TextView
-import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.control_layout.*
-import rx.observables.StringObservable
-import hu.akarnokd.rxjava.interop.RxJavaInterop
-import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.android.Main
 import java.io.IOException
-import java.io.InputStream
-import java.io.InputStreamReader
 import java.util.*
 
 class ControlActivity : AppCompatActivity() {
@@ -32,24 +28,78 @@ class ControlActivity : AppCompatActivity() {
         lateinit var m_bluetoothAdapter: BluetoothAdapter
         var m_isConnected: Boolean = false
         lateinit var m_address: String
+
+        var tip_stage: Int = -1
+        val tip_list: List<String> = listOf(
+                "Inicio da Baliza",
+                "Esterce totalmente o volante para o lado indicado",
+                "Gire o volante totalmente ao contrário",
+                "Ajuste o volante o necessário para finalizar")
+
+        var rotateLeft: Animation? = null
+        var rotateRight: Animation? = null
+        var blink: Animation? = null
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        actionBar?.hide()
         setContentView(R.layout.control_layout)
+
         m_address = intent.getStringExtra(SelectDeviceActivity.EXTRA_ADDRESS)
 
         ConnectToDevice(this).execute()
 
+        //imageView modifications
+        car_img.alpha = 0.2f
+
         switch_button.setOnClickListener { startStream(front_left, front_right, rear_left, rear_right) }
         control_led_disconnect.setOnClickListener { disconnect() }
+        next_step_btn.setOnClickListener{ changeState() }
+
+        rotateLeft = AnimationUtils.loadAnimation(this, R.anim.rotate)
+        rotateRight = AnimationUtils.loadAnimation(this, R.anim.rotate_right)
+        blink = AnimationUtils.loadAnimation(this, R.anim.blink)
 
     }
 
-    fun startStream(fl: TextView, fr: TextView, rl: TextView, rr: TextView) {
+    private fun changeState(){
+        if(tip_stage == 3)
+            tip_stage = 0
+        else
+            tip_stage++
+
+        when(tip_stage){
+            0 -> enterLeft()
+            1 -> enterRight()
+        }
+    }
+
+    private fun enterLeft(){
+        Log.i("STATE", tip_stage.toString())
+
+        left_arrow.alpha = 1.0f
+        right_arrow.alpha = 0.0f
+        left_arrow.startAnimation(blink)
+
+        steering_img.clearAnimation()
+        steering_img.startAnimation(rotateLeft)
+    }
+
+    private fun enterRight(){
+        Log.i("STATE", tip_stage.toString())
+
+        right_arrow.alpha = 1.0f
+        left_arrow.alpha = 0.0f
+        right_arrow.startAnimation(blink)
+
+        steering_img.clearAnimation()
+        steering_img.startAnimation(rotateRight)
+    }
+
+    private fun startStream(fl: TextView, fr: TextView, rl: TextView, rr: TextView) {
         if (m_bluetoothSocket != null) {
             GlobalScope.async(Dispatchers.Main){
-//                readFromArduino(m_bluetoothSocket!!.inputStream)
 
                 val scanner = Scanner(m_bluetoothSocket!!.inputStream)
                 var line: String
@@ -127,27 +177,5 @@ class ControlActivity : AppCompatActivity() {
             m_progress.dismiss()
         }
     }
-
-}
-
-private fun createObservable(inputStream: InputStream) {
-    val reader = InputStreamReader(inputStream)
-
-    RxJavaInterop.toV2Observable(StringObservable.byLine(StringObservable.from(reader)))
-            .map { it.split(" ")
-                    .map { it.toInt() }
-            }
-            .subscribeOn(Schedulers.computation())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe{numbers->
-                Log.i("Device", numbers.joinToString(" "))
-            }
-
-    /*Strings.from(reader, 15)
-            .subscribeOn(Schedulers.computation())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                    Log.i("device", it.toString())
-            }*/
 
 }
